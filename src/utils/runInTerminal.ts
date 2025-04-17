00e0cc8a71b2b1b8a6b68dab6ce83af2cb68c9be
Vishnu Vinod <vishnuvinod2772001@gmail.com>
2025-04-18 01:00:39 +0530

    Update runInTerminal function to set a default terminal name and ensure the terminal is shown.

--- src/utils/runInTerminal.ts
+++ src/utils/runInTerminal.ts
@@ -1,8 +1,9 @@
 import { window, env } from "vscode";
 
-export function runInTerminal(command: string, name?: string) {
+export function runInTerminal(command: string, name: string = "Angular Generator (CLI)"): Promise<boolean> {
     return new Promise<boolean>((resolve, reject) => {
         const terminal = window.createTerminal(name); // Use name if provided
+        terminal.show(); // Show the terminal
         const shell = env.shell; // Get the user's default shell
 
         // Adjust the command based on the shell
